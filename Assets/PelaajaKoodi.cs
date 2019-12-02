57764a678ecbe7be1618d21b958ca85eb672ba08
RavenousWhite <lindborgir@hotmail.com>
2019-12-02 15:56:41 +0200

    Hello Unity

---
+++ Assets/PelaajaKoodi.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PelaajaKoodi : MonoBehaviour
+{
+    public float nopeus;
+    Vector3 suunta;
+    Rigidbody RB;
+
+    void Start()
+    {
+    RB = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+    suunta.x = Input.GetAxis("Horizontal");
+    suunta.z = Input.GetAxis("Vertical");
+    RB.AddForce(suunta * nopeus);
+    }
+}
